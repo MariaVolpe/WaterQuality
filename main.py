109fe9ec026a78946e2fa9f4089c7d8ecbf03916
Maria Volpe <mariaCHVolpe@gmail.com>
2018-04-14 19:35:47 -0400

    Update main function to allow user to get average of a site using command line

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main():
     #load_obj.load_bacteria()
     #load_obj.load_sites()
     obj = calculate.Calculations()
+    # obj.generate_line_graph()
+    # obj.generate_bar_graph()
+    print("Enter a site name to get average data: ")
+    s = input()
+    print (obj.site(s, "W") )
+    print (obj.site(s, "D") )
 
 if __name__ == "__main__":
     main()
